5c2593b9023fb5a5494b2b709b41f024e43e44e2
jlmrlld <elladojana07@gmail.com>
2026-03-19 23:58:40 +0800

    first commit

---
+++ ellado-client/src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to React</h1>
+        <p>
+          Name: Juliana Marie Ellado <br />
+          Email: elladojb@students.national-u.edu.ph <br />
+          Course: BSIT-MWA <br />
+          Year: 3rd Year <br />
+
+          Github: @jlmrlld <br />
+          Hobbies: Playing video games, watching Kdramas, and listening to music. <br />
+        </p>
+      </header>
+    </div>
+  )
+}
+
+export default App;
