d4615da53bcfa883ed03ce9abe73284c920072bc
jlmrlld <elladojana07@gmail.com>
2026-05-24 21:11:20 +0800

    fix index

--- ellado-server/index.js
+++ ellado-server/index.js
@@ -29,7 +29,7 @@ const corsOptions = {
   preflightContinue: false,
   optionsSuccessStatus: 204, // For legacy browser support
 };
-app.options(" ", cors(corsOptions)); // Pre-flight request for all routes
+app.options("*", cors(corsOptions)); // Pre-flight request for all routes
 app.use(cors(corsOptions));
 
 // Curb Cores Error by adding a header here
