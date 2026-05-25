3db5ea7f44bd700f1c69e474c59543b287696f0d
jlmrlld <elladojana07@gmail.com>
2026-05-25 22:08:02 +0800

    fix index

--- ellado-server/index.js
+++ ellado-server/index.js
@@ -41,10 +41,10 @@ app.use((err, req, res, next) => {
   });
 });
 
-const PORT = process.env.PORT || 8000;
+// const PORT = process.env.PORT || 8000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+// app.listen(PORT, () => {
+  //console.log(`Server running on port ${PORT}`);
+//});
 
 module.exports = app;
